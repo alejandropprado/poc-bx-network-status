d58128b54e1ae2f506a5e47d65949ec5083dbdfe
alejandropprado <alejandropprado@gmail.com>
2023-07-31 10:08:38 -0400

    POC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,85 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [isOnline, setIsOnline] = useState<boolean | undefined>(undefined);
+  const [networkQuality, setNetworkQuality] = useState<
+    "desconocido" | "buena" | "media" | "mala" | "desconectado" | "excelente"
+  >("desconocido");
+
+  const handleOffline = () => {
+    setIsOnline(false);
+  };
+
+  const handleOnline = () => {
+    setIsOnline(true);
+  };
+
+  const checkNetworkQuality = (event: Event) => {
+    const downlink = (event?.target as any)?.downlink;
+
+    console.log({ event });
+
+    if (downlink === undefined) {
+      setNetworkQuality("desconocido");
+      return;
+    }
+
+    if (downlink === 0) {
+      setNetworkQuality("desconectado");
+      return;
+    } else if (downlink < 1) {
+      setNetworkQuality("mala");
+      return;
+    } else if (downlink < 2) {
+      setNetworkQuality("media");
+      return;
+    } else if (downlink < 3) {
+      setNetworkQuality("buena");
+      return;
+    } else if (downlink >= 3) {
+      setNetworkQuality("excelente");
+      return;
+    }
+  };
+
+  useEffect(() => {
+    const connection =
+      navigator?.connection ||
+      (navigator as any).mozConnection ||
+      (navigator as any).webkitConnection;
+
+    if (!connection) {
+      throw new Error("El navegador no soporta la API de conexión");
+    }
+
+    if (connection) {
+      connection.addEventListener("change", checkNetworkQuality);
+    }
+    setIsOnline(navigator?.onLine);
+    window.addEventListener("offline", handleOffline);
+    window.addEventListener("online", handleOnline);
+
+    return () => {
+      if (connection) {
+        connection.removeEventListener("change", checkNetworkQuality);
+      }
+      window.removeEventListener("offline", handleOffline);
+      window.removeEventListener("online", handleOnline);
+    };
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div>
+      {isOnline === undefined ? (
+        <p>Verificando conexión</p>
+      ) : isOnline ? (
+        <p>Conexión a Internet activa</p>
+      ) : (
+        <p>Sin conexión a Internet</p>
+      )}
+      <p>Calidad de la conexión: {networkQuality}</p>
+    </div>
+  );
 }
